248dfbede1faff23618047863fc92bb831e57ee2
vundelayamini <vundelayamini@gmail.com>
2021-04-28 20:24:30 +0530

    [yamini]Add: Welcome message to Collectons demo

---
+++ CollectoinsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectoinsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome message to Collections demo");
+        }
+    }
+}
